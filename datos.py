8cf2e411938bcb289e97cffb7967f9140afc7d46
canoludmila <ludmiicano@gmail.com>
2023-10-23 22:41:28 -0300

    Modificación de usuarios de ejemplo

--- datos.py
+++ datos.py
@@ -5,13 +5,13 @@ from profesor import Profesor
 estudiantes = [
     Estudiante("Jose", "Perez", "joseperez@gmail.com", "josesito1234", 54789, 2022), 
     Estudiante("Martin", "Rodriguez", "rodrimartin@gmail.com", "martinsito247", 50234, 2020),
-    Estudiante("Martin", "Rodriguez", "c", "d", 50234, 2020)
+    Estudiante("Martin", "Rodriguez", "mr@ejemplo.com", "martincho", 50234, 2020)
 ]
 
 profesores = [
     Profesor("Jorge", "Ramirez", "jorgeramirez@hotmail.com", "jorgitoalfajor", "Profesorado de Matematicas", 1998), 
     Profesor("Maria", "Tomaseti", "mariatomaseti@hotmail.com", "maria4566", "Tecnica en Programación", 2010),
-    Profesor("Jorge", "Ramirez", "a", "b", "Profesorado de Matematicas", 1998)
+    Profesor("Jorge", "Ramirez", "jr@ejemplo.com", "jorge", "Profesorado de Matematicas", 1998)
 ]
 
 cursos = [
